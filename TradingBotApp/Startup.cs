b4816b93cbb76fd3c3904702006c79c342eff357
Stas Lekontsev <stas.lekontsv@gmail.com>
2022-01-09 18:41:54 +0400

    Initial commit

---
+++ TradingBotApp/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TradingBotApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddControllersWithViews(MvcOptions =>
+            {
+                MvcOptions.EnableEndpointRouting = false;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvc(routes =>
+            {
+            });
+        }
+    }
+}
