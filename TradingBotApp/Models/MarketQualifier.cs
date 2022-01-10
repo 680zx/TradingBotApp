fcde417647abd5cd69d789074b1bbb40f14f4a10
Stas Lekontsev <stas.lekontsv@gmail.com>
2022-01-10 05:41:27 +0400

    Add Market Qualifier entity, add MarketProcessor nuget package

---
+++ TradingBotApp/Models/MarketQualifier.cs
@@ -0,0 +1,13 @@
+﻿using MarketProcessor.MarketConditionQualifiers.Interfaces;
+
+namespace TradingBotApp.Models
+{
+    public class MarketQualifier
+    {
+        public string? Name { get; set; }
+
+        public bool? Enabled { get; set; }
+
+        public IMarketConditionQualifier? MarketConditionQualifier { get; set; }
+    }
+}
