54d76b2aa150c811c2e6287bd114423be930a281
Stas Lekontsev <stas.lekontsv@gmail.com>
2022-01-10 21:17:24 +0400

    Add fake repository

---
+++ TradingBotApp/DAL/Implementation/FakeMarketToolRepository.cs
@@ -0,0 +1,16 @@
+﻿using TradingBotApp.DAL.Interfaces;
+using TradingBotApp.Models;
+
+namespace TradingBotApp.DAL.Implementation
+{
+    public class FakeMarketToolRepository : IMarketToolRepository
+    {
+        public IQueryable<MarketTool> MarketTools => new List<MarketTool>
+        {
+            new MarketTool { Name = "FakeTool1", Enabled = true },
+            new MarketTool { Name = "FakeTool2", Enabled = true },
+            new MarketTool { Name = "FakeTool3", Enabled = true },
+            new MarketTool { Name = "FakeTool4", Enabled = true },
+        }.AsQueryable();
+    }
+}
