0e0d2924ac5edb463ad233f872db8f933ebbc6e4
Stas Lekontsev <stas.lekontsv@gmail.com>
2022-01-21 03:16:00 +0400

    Update home controller and view

--- TradingBotApp/Controllers/HomeController.cs
+++ TradingBotApp/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TradingBotApp.DAL.Repositories.Interfaces;
 
 namespace TradingBotApp.Controllers
 {
     public class HomeController : Controller
     {
+        private ITradingBotAppRepository _repository;
+
+        public HomeController(ITradingBotAppRepository repository)
+        {
+            _repository = repository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_repository.MarketTools);
         }
     }
 }
