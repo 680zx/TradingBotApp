27fab8ddeac5a8b614db51bed481b6eb6c8ff951
Stas Lekontsev <stas.lekontsv@gmail.com>
2022-03-03 19:13:15 +0400

    Add abstract RepositoryBase class

---
+++ Contracts/Contracts/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using Entities;
+using System.Linq.Expressions;
+
+namespace Repository.Contracts
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext RepositoryContext { get; set; }
+
+        public RepositoryBase(RepositoryContext repositoryContext)
+        {
+            RepositoryContext = repositoryContext;
+        }
+
+        public void Create(T entity)
+        {
+            RepositoryContext.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            RepositoryContext.Set<T>().Remove(entity);
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return RepositoryContext.Set<T>();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return RepositoryContext.Set<T>().Where(expression);
+        }
+
+        public void Update(T entity)
+        {
+            RepositoryContext.Set<T>().Update(entity);
+        }
+    }
+}
